8461593acb3f5a350d8a48ea916477fcf8d71586
Matthew Skewer <139862120+Matthew-Skewer@users.noreply.github.com>
2023-08-04 12:17:19 +0200

    Add files from React4Component folder

---
+++ tasks and exercises/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExerciseComponent from './ExerciseComponent'
+import MyCmp from './ExerciseComponent';
+
+export default function App(){
+    return (<div>
+            <p>Todo: solve exercise</p>
+            <MyCmp></MyCmp>
+            </div>);
+}
---
+++ tasks and exercises/ExerciseComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function MyCmp(){
+    return <p>First exercise - done!</p>;
+}
+
