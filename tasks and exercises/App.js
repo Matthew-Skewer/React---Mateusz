8c9a20e4a6d99b52f2f14de41ef2e49deeca21c5
Matthew Skewer <139862120+Matthew-Skewer@users.noreply.github.com>
2023-08-05 11:20:46 +0200

    Manually upload React6 files

---
+++ tasks and exercises/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './styles.css';
+import Card from './Card'
+
+// don't change the Component name "App"
+export default function App() {
+    return (
+        <div>
+            <h1>Todos</h1>
+            
+            <Card>
+              <p>Please note: Below are just the most important todos - feel free to add more.</p>
+            </Card>
+            
+            <ul>
+              <li className="todo">
+                <Card>
+                  <h2>Learn React</h2>
+                  <p>Learn React fundamentals & explore core concepts</p>
+                </Card>
+              </li>
+              <li className="todo">
+                <Card>
+                  <h2>Practice React</h2>
+                  <p>Apply your knowledge & build demo projects</p>
+                </Card>
+              </li>
+            </ul>
+        </div>
+    );
+}
---
+++ tasks and exercises/Card.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Card(props){
+    return <div className ="card">{props.children}</div>
+}
+export default Card;
