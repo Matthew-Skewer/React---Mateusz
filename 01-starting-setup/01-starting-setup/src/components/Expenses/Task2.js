30e150de7ad9c1a41c5b1dce4c9552cf880bf41f
Mateusz Paszkiewicz <02mati.pasz@gmail.com>
2023-08-08 10:48:50 +0200

    Send a filter code

---
+++ 01-starting-setup/01-starting-setup/src/components/Expenses/Task2.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./Task2.css";
+
+const Task2 = (props) => {
+
+  const Dropdowner = (event) => {
+      props.onChangeFilter(event.target.value);
+  }
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={Dropdowner}>
+          <option value="2024">2024</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Task2;
